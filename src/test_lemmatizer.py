3e3ba34745515f5ed7455058e03daab302e47b4d
Ngo Van Tan Luu <luunvt@yes4all.com>
2023-12-11 17:00:10 +0700

    delete test file

--- src/test_lemmatizer.py
+++
@@ -1,9 +0,0 @@
-# make sure your downloaded the english model with "python -m spacy download en"
-
-import spacy
-nlp = spacy.load('en_core_web_sm')
-
-doc = nlp(u"gas")
-
-for token in doc:
-    print(token, token.lemma, token.lemma_)
